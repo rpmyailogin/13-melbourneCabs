1d5deda0266d6bb471545c6d9717c24700a825e6
Ritz <rp.myailogin@gmail.com>
2026-02-02 21:50:56 +1100

    Updated index.tsx

--- src/sections/AreasSection/index.tsx
+++ src/sections/AreasSection/index.tsx
@@ -1,71 +1,65 @@
+import { MapPin } from 'lucide-react';
+
+const areas = [
+  'Dandenong',
+  'Springvale',
+  'Hampton Park',
+  'Dandenong South',
+  'Rowville',
+  'Lyndhurst',
+  'Dandenong North',
+  'Endeavour Hills',
+  'Cranbourne',
+  'Noble Park',
+  'Doveton',
+  'Berwick',
+  'Keysborough',
+  'Hallam',
+  'Clyde North',
+  'Lynbrook'
+];
+
 export const AreasSection = () => {
   return (
-    <section className="box-border caret-transparent mb-[50px]">
-      <div className="box-border caret-transparent max-w-[1170px] w-full mx-auto px-[15px]">
-        <h2 className="text-[40px] font-bold box-border caret-transparent leading-[48px] text-center mb-[30px] font-poppins">
-          Areas We Serve
-        </h2>
-        <div className="box-border caret-transparent flex flex-wrap ml-[-15px] mr-[-15px]">
-          <div className="relative box-border caret-transparent basis-auto shrink max-w-none w-full px-[15px] md:basis-[33.3333%] md:shrink-0 md:max-w-[33.3333%]">
-            <ul className="box-border caret-transparent list-none mb-4">
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Dandenong
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Springvale
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Hampton Park
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Dandenong South
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Rowville
-              </li>
-            </ul>
-          </div>
-          <div className="relative box-border caret-transparent basis-auto shrink max-w-none w-full px-[15px] md:basis-[33.3333%] md:shrink-0 md:max-w-[33.3333%]">
-            <ul className="box-border caret-transparent list-none mb-4">
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Lyndhurst
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Dandenong North
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Endeavour Hills
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Cranbourne
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Noble Park
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Doveton
-              </li>
-            </ul>
-          </div>
-          <div className="relative box-border caret-transparent basis-auto shrink max-w-none w-full px-[15px] md:basis-[33.3333%] md:shrink-0 md:max-w-[33.3333%]">
-            <ul className="box-border caret-transparent list-none mb-4">
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Berwick
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Keysborough
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Hallam
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Clyde North
-              </li>
-              <li className="text-[25px] bg-[url('https://www.dandenongtaxicabs.com/img/tick.png')] bg-no-repeat bg-size-[15px] box-border caret-transparent leading-[30px] bg-[position:0px_13px] mb-[15px] pl-6 pt-[7px] pb-2">
-                Lynbrook
-              </li>
-            </ul>
-          </div>
+    <section className="py-20 bg-gradient-to-b from-gray-50 to-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center mb-16">
+          <h2 className="text-4xl md:text-5xl font-bold text-gray-900 mb-4">
+            Areas We Serve
+          </h2>
+          <p className="text-lg text-gray-600 max-w-2xl mx-auto">
+            Reliable taxi service across Melbourne's southeast suburbs
+          </p>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          {areas.map((area, index) => (
+            <div
+              key={index}
+              className="group relative bg-white rounded-xl p-6 shadow-sm hover:shadow-xl transition-all duration-300 border border-gray-100 hover:border-yellow-400 hover:-translate-y-1"
+            >
+              <div className="flex items-center gap-3">
+                <div className="flex-shrink-0 w-10 h-10 bg-yellow-400 rounded-lg flex items-center justify-center group-hover:scale-110 transition-transform duration-300">
+                  <MapPin className="w-5 h-5 text-gray-900" />
+                </div>
+                <h3 className="text-lg font-semibold text-gray-900 group-hover:text-yellow-600 transition-colors duration-300">
+                  {area}
+                </h3>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="mt-12 text-center">
+          <p className="text-gray-600 mb-6">
+            Don't see your area? Give us a call and we'll be happy to assist you!
+          </p>
+          <a
+            href="#contact"
+            className="inline-block bg-yellow-400 text-gray-900 font-semibold px-8 py-3 rounded-lg hover:bg-yellow-500 transition-colors duration-300 shadow-md hover:shadow-lg"
+          >
+            Contact Us
+          </a>
         </div>
       </div>
     </section>
